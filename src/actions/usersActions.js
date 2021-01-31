1e54efa2d6b9dfdface0f3ba41025944b985e58e
josearc <juanjose.arcani2309@gmail.com>
2021-01-31 17:31:30 -0400

    try and catch implementados

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -2,13 +2,17 @@ import axios from 'axios'
 import { TRAER_TODOS } from '../types/usersTypes'
 
 export const traerTodos = () => async (dispatch) => {
-  const response =  await axios.get('https://jsonplaceholder.typicode.com/users')
-  dispatch({
-    type: TRAER_TODOS,
-    payload: {
-      loading: false,
-      error: null,
-      users: response.data,
-    }
-  })
+  try {
+    const response =  await axios.get('https://jsonplaceholder.typicode.com/users')
+    dispatch({
+      type: TRAER_TODOS,
+      payload: {
+        loading: false,
+        error: null,
+        users: response.data,
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
 }
